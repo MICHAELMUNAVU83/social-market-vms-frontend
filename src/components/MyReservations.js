537932725ae7f90f665de68010c1a2304f9718eb
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-12-10 16:32:48 +0300

    Fixed delete bug

--- src/components/MyReservations.js
+++ src/components/MyReservations.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "../MyReservations.css";
 
 function MyReservations({ currrentUserName, currentUserId }) {
   const [reservations, setReservations] = useState([]);
@@ -10,7 +11,7 @@ function MyReservations({ currrentUserName, currentUserId }) {
       .then((data) => {
         setReservations(data);
       });
-  }, [currentUserId]);
+  }, [currentUserId, reservations]);
 
   const handleDelete = (id) => {
     fetch(
@@ -29,50 +30,58 @@ function MyReservations({ currrentUserName, currentUserId }) {
           });
       });
     });
-    alert("reservations deleted");
   };
 
   const MyReservations = reservations.map((reservation) => (
     <div className="card my-4">
-      <div className="card-header text-center">
+      <div className="card-header text-center top-reservation-details">
         Here are the details of your vending reservation for{" "}
         {reservation.event.name}
       </div>
-      <div className="d-flex justify-content-center  container">
-        <div className="card-body">
+      <div className="d-flex justify-content-around">
+        <div>
           <h5 className="card-title">
             <span>Event: </span>
-            {reservation.event.name}
+            <span>{reservation.event.name}</span>
           </h5>
           <p className="card-text">
-            <span>Event Date: </span>
-            {reservation.event.date}
+            <span className="reservation-title">Event Venue : </span>
+            <span className="reservation-span">{reservation.event.venue}</span>
           </p>
           <p className="card-text">
-            <span>Event Venue : </span>
-            {reservation.event.venue}
+            <span className="reservation-title">Event Date: </span>
+            <span className="reservation-span">{reservation.event.date}</span>
           </p>
+
           <p className="card-text">
-            <span>Time : </span>
-            {reservation.event.time_event_starts}
+            <span className="reservation-title">Time : </span>
+            <span className="reservation-span">
+              {reservation.event.time_event_starts}
+            </span>
           </p>
         </div>
-        <div className="card-body">
+        <div>
           <h5 className="card-title">
             <span>Vending Category: </span>
-            {reservation.vendor_category.category}
+            <span>{reservation.vendor_category.name}</span>
           </h5>
           <p className="card-text">
-            <span>Vendor Passes: </span>
-            {reservation.vendor_category.vendor_passes_per_slot}
+            <span className="reservation-title">Vendor Passes: </span>
+            <span className="reservation-span">
+              {reservation.vendor_category.vendor_passes_per_slot}
+            </span>
           </p>
           <p className="card-text">
-            <span>Ammenities: </span>
-            {reservation.vendor_category.amenities_provided}
+            <span className="reservation-title">Ammenities: </span>
+            <span className="reservation-span">
+              {reservation.vendor_category.amenities_provided}
+            </span>
           </p>
           <p className="card-text">
-            <span>Cost: </span>
-            {reservation.vendor_category.cost_per_slot}
+            <span className="reservation-title">Cost: </span>
+            <span className="reservation-span">
+              Ksh {reservation.vendor_category.cost_per_slot}
+            </span>
           </p>
         </div>
       </div>
@@ -84,7 +93,7 @@ function MyReservations({ currrentUserName, currentUserId }) {
           Delete
         </button>
       </div>
-      <div className="card-footer text-center text-muted">
+      <div className="card-footer text-center text-muted bottom-reservation-details">
         You will be contacted to pay the reservation fee of{" "}
         {reservation.vendor_category.cost_per_slot}
       </div>
